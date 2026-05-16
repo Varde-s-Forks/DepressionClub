d3a51c09aa0112ad377edbb6d46930802a161fb0
Vardë <ichunjo.le.terrible@gmail.com>
2026-05-16 20:14:53 +0200

    dsqdqs

--- .github/cmake-cache/onnxruntime-macos.cmake
+++ .github/cmake-cache/onnxruntime-macos.cmake
@@ -5,3 +5,6 @@ set(CMAKE_BUILD_TYPE Release CACHE STRING "")
 set(onnxruntime_ENABLE_CPU_FP16_OPS ON CACHE BOOL "")
 set(onnxruntime_USE_COREML ON CACHE BOOL "")
 set(CMAKE_OSX_ARCHITECTURES "arm64" CACHE STRING "")
+
+# Compatibility with CMake 4.x for dependencies using old CMake versions
+set(CMAKE_POLICY_VERSION_MINIMUM 3.5 CACHE STRING "")
